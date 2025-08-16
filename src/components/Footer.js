39b4a9c8fed299e04d821fa999f153696ea6ef54
muto <msupri29@gmail.com>
2025-08-17 04:34:39 +0700

    add new fiture

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
         {/* Bagian link sosial media */}
         <div className="flex flex-col md:flex-row gap-2 md:gap-4 text-center md:text-left mb-12 sm:mb-0">
           <a
-            href="https://github.com/username"
+            href="https://github.com/Muto7"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:underline"
@@ -21,7 +21,7 @@ const Footer = () => {
             GitHub
           </a>
           <a
-            href="https://linkedin.com/in/username"
+            href="https://www.linkedin.com/in/muchamad-supriyanto/"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:underline"
